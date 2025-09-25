0d2682e260148effa0004320bc819a66a2a9121e
arielpapini0-lgtm <arielpapini0@gmail.com>
2025-09-25 15:29:34 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,22 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-      caches.open('dojo-chronos').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/script.js',
-          '/assets/hit.mp3',
-          '/assets/pause.mp3',
-          '/assets/end.mp3',
-          '/assets/firma.png',
-          '/assets/icon-192.png',
-          '/assets/icon-512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', e => {
-    e.respondWith(
-      caches.match(e.request).then(response => response || fetch(e.request))
-    );
-  });
+const CACHE_NAME = 'point-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
